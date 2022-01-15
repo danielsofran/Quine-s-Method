af9f49f26142829800acfefd0de2096a4b857a62
Daniel <daniel.sofran@stud.ubbcluj.ro>
2022-01-15 02:06:50 +0200

    Nightcore

---
+++ test_monom.py
@@ -0,0 +1,16 @@
+from monom import Monom
+
+def test_constr():
+    m = Monom(12)
+    assert m[1] == 1
+    assert m[2] == 1
+    assert m[3] == 0
+    assert m[4] == 0
+    assert int(m) == 12
+    assert m.nr1() == 2
+    n = Monom([1, 0, 0, 0])
+    p = m + n
+    assert p.nivel_factorizare() == 1
+
+def test():
+    test_constr()
