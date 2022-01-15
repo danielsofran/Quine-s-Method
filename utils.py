8fa38ddee3e18794b7b16f82dc8844eb6b4b4d6f
Daniel <92435488+danielsofran@users.noreply.github.com>
2022-01-15 09:15:51 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ def afisare(l):
 
 def citire():
     Monom.ordin = int(input("Introduceti numarul de variabile x: "))
-    #inputstr = input("Introduceti mintermii in baza 10 sau 2: ")
+    inputstr = input("Introduceti mintermii in baza 10 sau 2: ")
 
     #inputstr = "15 13 14 10 2 4"
-    inputstr = "1 3 4 5 6 7 8 9 10 11 12 14"
+    #inputstr = "1 3 4 5 6 7 8 9 10 11 12 14"
     _ = inputstr.split()
     rez = []
     for __ in _:
